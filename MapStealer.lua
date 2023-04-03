4578fbbefe02c9723c6163f34bfc36fdf89b11d8
gobmop1 <123776687+gobmop1@users.noreply.github.com>
2023-04-03 10:39:21 -0600

    Add files via upload

---
+++ MapStealer.lua
@@ -0,0 +1,8 @@
+print("Thanks for using Viper Gutzz#4245 map stealer"
+    local Workspace = game.workspace
+        if Workspace = false then
+            print("Nothing to copy")
+        else print ("Map found")
+    wait()
+end
+saveinstance()
